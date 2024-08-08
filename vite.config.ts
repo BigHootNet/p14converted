a1537933cf5c031d152433903de39dc90d1bba41
BigHootNet <thetrigun@gmail.com>
2024-08-08 17:03:58 +0200

    v0.3

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
+  const base = mode === 'production' ? '/p14converted/' : '/';
+
   return {
     plugins: [react()],
-    base: '/',
+    base: base,
     build: {
       outDir: 'dist',
       rollupOptions: {
